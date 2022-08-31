2e0dc56f4e02e72302da627fa97a5207711f3f4e
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-08-31 21:21:09 +0300

    Installing Vue Js with his libraries

---
+++ 0_Notes_of_Ammar_via_learning/Vue.cmd
@@ -0,0 +1,13 @@
+npm install vue@next vue-loader@next
+@vue/compiler-sfc
+babel-loader
+@babel/core
+@babel/preset-env
+vue-router@next
+
+npm install vue@next vue-loader@next
+npm i @vue/compiler-sfc
+npm i babel-loader
+npm i @babel/core
+npm i @babel/preset-env
+npm i vue-router@next
